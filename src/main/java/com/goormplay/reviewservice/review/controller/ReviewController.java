7709d349e70fd37aab8a862be33b611d996a350d
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-05-19 15:39:14 +0900

    fix: Optimizing API Gateway path (#39)
    클라이언트 → Gateway 요청
    예: /api/public/contents/latest
    1. CustomPreFilter 실행
    X-Original-Path: /api/public/contents/latest 헤더 추가
    2. Gateway RewritePath
    실제 요청 URI는 /contents/latest로 마이크로서비스에 전달
    3. JWT Filter에서 X-Original-Path 확인
    permit-paths 기준으로 허용 여부 체크

--- src/main/java/com/goormplay/reviewservice/review/controller/ReviewController.java
+++ src/main/java/com/goormplay/reviewservice/review/controller/ReviewController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/review")
+@RequestMapping("/review")
 @RequiredArgsConstructor
 public class ReviewController {
 
